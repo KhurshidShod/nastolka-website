f6bb3de2068ecb4ee0970bd8452c89cb1383a24f
Xurshidbek Shodiyev <xurshidbekshodiyev@Xurshidbeks-MacBook-Pro.local>
2024-09-06 21:05:15 +0500

    Home Page Final Animations added

--- src/App.vue
+++ src/App.vue
@@ -19,10 +19,18 @@ export default {
     };
   },
   mounted() {
-    // Set up the window.onload event to hide the loader once the page is fully loaded
-    window.onload = () => {
+    // Listen to the window load event
+    window.addEventListener('load', this.handleWindowLoad);
+  },
+  methods: {
+    handleWindowLoad() {
+      // Once the page has fully loaded, remove the loading spinner
       this.isLoading = false;
-    };
+    },
+  },
+  beforeDestroy() {
+    // Clean up the event listener
+    window.removeEventListener('load', this.handleWindowLoad);
   },
 }
 </script>
