0c95f4a9f76c5370831459adb233763a61dde3f8
Xurshidbek Shodiyev <xurshidbekshodiyev@Xurshidbeks-MacBook-Pro.local>
2024-09-14 03:03:37 +0500

    asd

--- src/main.js
+++ src/main.js
@@ -3,14 +3,8 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./routes";
 import Button from "./components/Reusables/Button.vue";
-import VueLazyload from "vue-lazyload";
 const app = createApp(App);
 
 app.component("MainButton", Button);
-app.use(router, VueLazyload);
-app.use(VueLazyload, {
-  preLoad: 1.3,
-  loading: "./assets/gifs/image_processing20210904-17661-1in8afj.gif",
-  attempt: 1,
-});
+app.use(router);
 app.mount("#app");
