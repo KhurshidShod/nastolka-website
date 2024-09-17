72997eea8c8c828c5748da45d673d6c2bf2f866d
Xurshidbek Shodiyev <xurshidbekshodiyev@Xurshidbeks-MacBook-Pro.local>
2024-09-17 17:20:50 +0500

    Final changes added

--- src/main.js
+++ src/main.js
@@ -3,15 +3,8 @@ import "./style.css";
 import App from "./App.vue";
 import router from "./routes";
 import Button from "./components/Reusables/Button.vue";
-import VueLazyload from "vue3-lazyload";
 const app = createApp(App);
 
 app.component("MainButton", Button);
 app.use(router);
-app.use(VueLazyload, {
-  preLoad: 1.3,
-  error: "error-image.png",
-  loading: "loading-spinner.gif",
-  attempt: 1,
-});
 app.mount("#app");
